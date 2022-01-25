40af28b0e1c25eff3859d7f0c42a99f5942c3059
Matthew Priebe <mbpriebe@madisoncollege.edu>
2022-01-25 15:08:25 -0600

    created remove buttons that interact with redux store and can remove items from the list.

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
-import ExpenseListItem, { ExpenseListItem2 } from "./ExpenseListItem";
+import ExpenseListItem from "./ExpenseListItem";
 import selectExpenses from "../selectors/expenses";
 
 const ExpenseList = (props) => (
     <div>
-        <h1>Expense List</h1>
         {
-            props.expenses.map((item) => (
+            /**
+             * Alternate Way to get items and loop. other way used down below.
+             *
+             props.expenses.map((item) => (
                 <ExpenseListItem
                     key={item.id}
                     description={item.description}
                     amount={item.amount}
                     createdAt={item.createdAt}
                 />
-            ))
+             ))
+             */
         }
-
         <h1>Expense List v.2</h1>
         {
             props.expenses.map((item) => (
-                <ExpenseListItem2 key={item.id} {...item} />
+                <ExpenseListItem key={item.id} {...item} />
             ))
         }
     </div>
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { removeExpense } from "../actions/expenses";
+import { connect } from "react-redux";
 
-const ExpenseListItem = (props) => (
+/**
+ *Alternative way to list out items.
+ */
+const ExpenseListItem_old = (props) => (
     <div>
         <h3>{props.description}</h3>
         <p>{props.amount} --- {props.createdAt}</p>
     </div>
 );
 
-export const ExpenseListItem2 = ({ description, amount, createdAt }) => (
+//dispatch is in the props. same as props.dispatch
+const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
     <div>
         <h3>{description}</h3>
         <p>{amount} --- {createdAt}</p>
+        <button onClick={(e) => {
+            dispatch(removeExpense({ id: id }))
+            console.log(id);
+        }}>Remove</button>
     </div>
 );
 
-export default ExpenseListItem ;
+export default connect()(ExpenseListItem);
