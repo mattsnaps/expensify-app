930207272c254fafcb62303bf81be34b24d2a97e
Matthew Priebe <mbpriebe@madisoncollege.edu>
2022-01-28 15:13:55 -0600

    Added all tests for the expense reducers

---
+++ src/tests/reducers/expenseReducer.test.js
@@ -0,0 +1,73 @@
+import expensesReducer from "../../reducers/expensesReducer";
+import expenses from "../fixtures/expenses";
+
+test('expensesReducer set default state Success', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
+
+test('expensesReducer remove expense by id Success', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('expensesReducer remove expense by NOT FOUND id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1233'
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
+
+test('expenseReducer add an expense Success', () => {
+    const newExpense = {
+        id: '344',
+        description: 'Bear',
+        note: '',
+        amount: 19533,
+        createdAt: 0
+    }
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense: newExpense
+    }
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([...expenses, newExpense]);
+});
+
+test('expenseReducer edit an expense Success', () => {
+    const testNote = 'This is a test';
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: expenses[1].id,
+        updates: {
+            note: testNote
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state[1].note).toEqual(testNote);
+});
+
+test('expenseReducer edit a NOT FOUND expense', () => {
+    const testNote = 'This is a test';
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: 300,
+        updates: {
+            note: testNote
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
+
+
+
+
+
+
--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,25 +1,6 @@
 import getVisibleExpenses from "../../selectors/expenses";
 import moment from "moment";
-
-const expenses = [{
-    id: '1',
-    description: 'Gum',
-    note: '',
-    amount: 195,
-    createdAt: 0
-}, {
-    id: '2',
-    description: 'Rent',
-    note: '',
-    amount: 1095,
-    createdAt: moment(0).subtract(4, 'day').valueOf()
-}, {
-    id: '3',
-    description: 'Credit Card',
-    note: '',
-    amount: 4050,
-    createdAt: moment(0).add(4, 'day').valueOf()
-}];
+import expenses from "../fixtures/expenses";
 
 test('getVisibleExpenses Filter by text value Success', () => {
     const filters = {
